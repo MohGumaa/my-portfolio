18ccdec78453f161dd51429ebc60e19d1da668c2
MohGumaa <gomaam14@gmail.com>
2020-04-03 02:00:04 +0530

    Updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.querySelector('.menu-btn').addEventListener('click', () =>{
   document.querySelector('.main-small').classList.toggle('show');
-  const showMenu = setTimeout(() => document.querySelector('.main-small').classList.remove('show'), 3000)
-  clearInterval(showMenu);
+  // const showMenu = setTimeout(() => document.querySelector('.main-small').classList.remove('show'), 3000)
+  // clearInterval(showMenu);
 })
