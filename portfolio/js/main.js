1a30a2f292f9e0366add13246066e7cebddb7ae3
MohGumaa <gomaam14@gmail.com>
2020-04-04 01:40:58 +0530

    Update Responsive layout for site

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,5 +1,10 @@
-document.querySelector('.menu-btn').addEventListener('click', () =>{
-  document.querySelector('.main-small').classList.toggle('show');
-  const showMenu = setTimeout(() => document.querySelector('.main-small').classList.remove('show'), 3000)
-  clearInterval(showMenu);
-})
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.menu-btn').addEventListener('click', () =>{
+    document.querySelector('.main-small').classList.toggle('show');
+    document.querySelectorAll('.page-show').forEach(button => {
+      button.onclick = () => {
+        document.querySelector('.main-small').classList.toggle('show')
+    };
+  });
+  })
+});
