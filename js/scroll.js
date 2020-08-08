2c208254da26c529bbcb7d5ac2f661cc7a2c9fd6
MohGumaa <gomaam14@gmail.com>
2020-08-08 19:57:12 +0300

    Updates

---
+++ js/scroll.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  $("a.scroll").on("click", function (e) {
+    if (this.hash !== "") {
+      e.preventDefault();
+
+      const hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800
+      );
+    }
+  });
+
+  $(window).scroll(function () {
+    if (this.scrollY > this.innerHeight / 2) {
+      $(".scroll-to-top").css("display", "block");
+    } else {
+      $(".scroll-to-top").css("display", "none");
+    }
+  });
+
+  //   Nav
+  $(".menu-btn").on("click", function () {
+    $(".main-small").toggleClass("show");
+  });
+});
